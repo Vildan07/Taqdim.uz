06c0738a8e10d6b429d74de2cace56aa38fd272c
Vildan Mullagaliyev <mullagalievvildan07@gmail.com>
2024-08-01 13:03:32 +0500

    add detail username endpoint

--- userapp/urls.py
+++ userapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
 
     path('profile/create/', UserProfileCreateAPIView.as_view(), name='profile-create'),
-    path('profile/<str:username>/', UserProfileDetailAPIView.as_view(), name='profile-detail'),
+    path('profile/<str:username>/detail', UserProfileDetailAPIView.as_view(), name='profile-detail'),
     path('profile/', UserProfileUpdateDeleteAPIView.as_view(), name='profile-update-delete'),
 ]
